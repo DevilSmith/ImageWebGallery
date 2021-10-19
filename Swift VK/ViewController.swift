b45ebdc1838283e30407c4ac284540615c4a89a4
DevilSmith <hellsbadmans@gmail.com>
2021-10-19 21:01:41 +0300

    Initial Commit

---
+++ Swift VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift VK
+//
+//  Created by Stanislav Briver on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
